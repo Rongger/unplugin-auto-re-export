3b026016aaaef5cc742442730730638201ab6b26
johnny <934004841@qq.com>
2022-06-20 16:28:32 +0800

    fix: cjs import

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,28 +1,37 @@
-import { basename, resolve } from 'path'
-import { promises as fs } from 'fs'
-import fg from 'fast-glob'
-import chalk from 'chalk'
+import { basename, resolve } from "path";
+import { promises as fs } from "fs";
+import fg from "fast-glob";
+import chalk from "chalk";
 
 async function run() {
-  const files = await fg('*.js', {
-    ignore: ['chunk-*'],
+  const files = await fg("*.js", {
+    ignore: ["chunk-*"],
     absolute: true,
-    cwd: resolve(__dirname, '../dist'),
-  })
+    cwd: resolve(__dirname, "../dist"),
+  });
+
   for (const file of files) {
     // eslint-disable-next-line no-console
-    console.log(chalk.cyan.inverse(' POST '), `Fix ${basename(file)}`)
-    if (file === 'index.js') {
-      // fix cjs exports
-      let code = await fs.readFile(file, 'utf8')
-      code = code.replace('exports.default =', 'module.exports =')
-      code += 'exports.default = module.exports;'
-      await fs.writeFile(file, code)
-    }
+    console.log(chalk.cyan.inverse(" POST "), `Fix ${basename(file)}`);
+
+    let code = await fs.readFile(file, "utf8");
+    code = code.replace(
+      'var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);',
+      'var __toCommonJS = (mod) => __copyProps(__defProp(mod.default ?? {}, "__esModule", { value: true }), mod);'
+    );
+    const moduleExports = code.match(/^module.exports = .*$/m)?.[0];
+    if (moduleExports === undefined)
+      throw new Error("module.exports is not defined");
+    code += moduleExports;
+    await fs.writeFile(file, code);
+
     // generate submodule .d.ts redirecting
-    const name = basename(file, '.js')
-    await fs.writeFile(`${name}.d.ts`, `export { default } from './dist/${name}'\n`)
+    const name = basename(file, ".js");
+    await fs.writeFile(
+      `${name}.d.ts`,
+      `export { default } from './dist/${name}'\n`
+    );
   }
 }
 
-run()
+run();
